fd44de5a95efe7ab0e43cfc7793bb4147573d426
Marcus Kraßmann <marcus.krassmann@gmail.com>
2023-03-20 17:57:24 +0100

    Added spanish to hello

--- 01-hello-world/hello.go
+++ 01-hello-world/hello.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + ", " + name
+	var greeting string
+	if language == "Spanish" {
+		greeting = spanishHelloPrefix
+	} else {
+		greeting = englishHelloPrefix
+	}
+	return greeting + ", " + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
