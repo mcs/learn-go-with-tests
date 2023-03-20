5bb369742e64ef53175aa645ddc82b0df44e1de9
Marcus Kraßmann <marcus.krassmann@gmail.com>
2023-03-20 19:55:45 +0100

    Added boundary tests for SumAllTails

--- 04-arrays-slices/sum_test.go
+++ 04-arrays-slices/sum_test.go
@@ -42,8 +42,12 @@ func TestSumAllTails(t *testing.T) {
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
-		got := SumAllTails([]int{}, []int{3, 4, 5})
-		want := []int{0, 9}
+		got := SumAllTails(
+			[]int{},
+			[]int{1},
+			[]int{1, 2},
+			[]int{3, 4, 5})
+		want := []int{0, 0, 2, 9}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
