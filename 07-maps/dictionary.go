dc8207eb41c66a611f16f90726dec13dd23b4a3d
Marcus Kraßmann <marcus.krassmann@gmail.com>
2023-03-20 23:13:07 +0100

    Return error on Add() if word exists

--- 07-maps/dictionary.go
+++ 07-maps/dictionary.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("todo")
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -14,6 +15,10 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(key, value string) {
-	d[key] = value
+func (d Dictionary) Add(key, value string) error {
+	if _, ok := d[key]; !ok {
+		d[key] = value
+		return nil
+	}
+	return ErrWordExists
 }
