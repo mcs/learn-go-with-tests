a455e374e4bdc82cb29d7d6af4b9b5d24ad72104
Marcus Kraßmann <marcus.krassmann@gmail.com>
2023-03-20 23:30:37 +0100

    Refactored error messages and parameter names

--- 07-maps/dictionary.go
+++ 07-maps/dictionary.go
@@ -10,8 +10,8 @@ func (e DictionaryErr) Error() string {
 
 const (
 	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists       = DictionaryErr("todo")
-	ErrWordDoesNotExist = DictionaryErr("")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 func (d Dictionary) Search(word string) (string, error) {
@@ -22,11 +22,11 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(key, value string) error {
-	_, err := d.Search(key)
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
 	switch err {
 	case ErrNotFound:
-		d[key] = value
+		d[word] = definition
 	case nil:
 		return ErrWordExists
 	default:
