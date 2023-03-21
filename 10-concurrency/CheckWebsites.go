753c465d7f29d45a65bf8e8ce6be931c211f9354
Marcus Kraßmann <marcus.krassmann@gmail.com>
2023-03-21 20:27:03 +0100

    3rd try of using Go routines (sometimes works, also unnecessarily slow)

--- 10-concurrency/CheckWebsites.go
+++ 10-concurrency/CheckWebsites.go
@@ -8,6 +8,7 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
+		url := url
 		go func() {
 			results[url] = wc(url)
 		}()
