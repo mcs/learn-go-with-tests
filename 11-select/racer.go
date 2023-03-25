8d761dde812172451a85950c63f413b57558eae2
Marcus Kraßmann <marcus.krassmann@gmail.com>
2023-03-25 10:14:25 +0100

    Switched implementation to use select and channels

--- 11-select/racer.go
+++ 11-select/racer.go
@@ -2,23 +2,22 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
-		winner = a
-	} else {
-		winner = b
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
 	}
-	return
 }
 
-func measureResponseTime(url string) time.Duration {
-	startA := time.Now()
-	http.Get(url)
-	return time.Since(startA)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
